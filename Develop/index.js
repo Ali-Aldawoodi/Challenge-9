6e30778254f9cf59dc9ebf480188050712eb8a04
Ali Aldawoodi <124013342+Ali-Aldawoodi@users.noreply.github.com>
2023-08-23 21:01:01 -0600

    Imported inquirer. Deleted cloned README. Added questions inside questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,40 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'Title'
+    },
+    {
+        type:'input',
+        message: 'What is the description of your project?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What is the usage information?',
+        name: 'usage information',
+    },
+    {
+        type: 'input',
+        message: 'What are your projects contribution guidelines?',
+        name: 'guidelines',
+    },
+    {
+        type: 'input',
+        message: 'What are the test intructions?',
+        name: 'test',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
